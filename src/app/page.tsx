fb441f197a07b2c2743f72fa2fbf8f950b701089
coderhisham <imhish4m@gmail.com>
2024-06-17 11:52:10 +0530

    Switched to Next.JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import "./page.css";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="">Coming Soon...</h1>
-      <h3> Stay Tuned!!!</h3>
+    <main className="flex min-h-screen flex-col items-center justify-center p-15">
+      <h1>Coming Soon...</h1>
+      <h3>Stay Tuned!!!</h3>
     </main>
   );
 }
